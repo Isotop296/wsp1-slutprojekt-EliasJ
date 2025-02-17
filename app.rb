f162c3377bf60487263cd4b013685d2f0c6a73d4
Elias-Jadefrid <elias.jadefrid@elev.ga.ntig.se>
2025-02-17 13:21:04 +0100

    början
    Har lagt in enkel database och ska nu påbörja första sidan, som jag tror kommer vara login

--- app.rb
+++ app.rb
@@ -1,7 +1,26 @@
+
 class App < Sinatra::Base
+
+    def db
+        return @db if @db
+
+        @db = SQLite3::Database.new("db/spel.sqlite")
+        @db.results_as_hash = true
+
+        return @db
+    end
+
+    def UserSessionManager(userId)
+        active_user = session[:userId]
+    end
 
     get '/' do
         erb(:"index")
     end
 
+
+    
+
 end
+
+
--- db/seeder.rb
+++ db/seeder.rb
@@ -1,9 +1,43 @@
 require 'sqlite3'
+require 'bcrypt'
+
 
 class Seeder
 
   def self.seed!
-    p "doit"
+    drop_tables
+    create_tables
+    populate_tables
+  end
+
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS users')
+  end
+
+  def self.create_tables
+     db.execute('CREATE TABLE users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT NOT NULL,
+      password integer,
+      description TEXT NOT NULL,
+      cuntry TEXT NOT null,
+      city TEXT NOT Null,
+      admin text
+      )')
+
+  end
+
+  def self.populate_tables
+    db.execute('INSERT INTO users (username, password, cuntry, city, admin ) VALUES ("Flusek", 296, "Jag heter elias.", "Sweden", "Gothenburg", True)')
+
+  end
+
+  private
+  def self.db
+    return @db if @db
+    @db = SQLite3::Database.new('db/spel.sqlite')
+    @db.results_as_hash = true
+    @db
   end
 
 end
